52634dcc5c6699347e0993494a8da73d59bd556f
Bruna Stephane da Silva <73487831+Brunastephane@users.noreply.github.com>
2023-02-14 18:30:41 +0000

    API Page Books

--- src/pages/Books/Books.tsx
+++ src/pages/Books/Books.tsx
@@ -1,9 +1,26 @@
 import { Container, Grid, Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DisplayBook from "../../components/Book/DisplayBook";
 import { booksQuery } from "../../components/Book/BooksQuery";
 
 function Books() {
+  /*
+  const [booksQuery, setBooksQuery] = useState<string[]>();
+
+  const getBooks = async () => {
+    await fetch("http://localhost:4000/booksFromAPI")
+      .then((response) => response.json())
+      .then((res) => {
+        setBooksQuery(res);
+      });
+  };
+
+  useEffect(() => {
+    if (!booksQuery) {
+      getBooks();
+    }
+  }, [booksQuery]);
+  */
   return (
     <Container>
       <Typography sx={{ paddingTop: "40px" }} variant="h4">
@@ -11,7 +28,10 @@ function Books() {
       </Typography>
 
       <Grid container spacing={2} sx={{ paddingTop: "40px" }}>
-        {booksQuery.map((book, index) => DisplayBook(book, index))}
+        {booksQuery &&
+          booksQuery.map((book, index) => (
+            <DisplayBook key={index} index={index} book={book} />
+          ))}
       </Grid>
     </Container>
   );
